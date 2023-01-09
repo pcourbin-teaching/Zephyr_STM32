2d1fb06ddbda660645132c0adfe08399a9cdc9aa
Pierre COURBIN <pierre.courbin@gmail.com>
2023-01-09 10:09:15 +0100

    Update GUI-BME

--- Zephyr_WithGUI_WithBME/src/main.c
+++ Zephyr_WithGUI_WithBME/src/main.c
@@ -8,14 +8,14 @@
 #include <kernel.h>
 #include <device.h>
 #include <drivers/display.h>
-#include <drivers/gpio.h>
-
 #include <drivers/sensor.h>
-#include <stdio.h>
+#include <drivers/gpio.h>
 
 #include <lvgl.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
 
 
 #define DISPLAY_DEV_NAME DT_LABEL(DT_INST(0, ilitek_ili9340))
@@ -26,8 +26,9 @@ LOG_MODULE_REGISTER(app);
 
 
 /* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   1000
+#define SLEEP_TIME_MS   100
 
+#include "ESIEALogo.h"
 
 static void btn_event_cb(lv_obj_t * btn, lv_event_t event)
 {
@@ -49,12 +50,41 @@ lv_obj_t * btn = NULL;
 void lv_ex_get_started_1(void)
 {
     btn = lv_btn_create(lv_scr_act(), NULL);     /*Add a button the current screen*/
-    lv_obj_set_pos(btn, 10, 10);                            /*Set its position*/
+    lv_obj_set_pos(btn, 60, 100);                            /*Set its position*/
     lv_obj_set_size(btn, 120, 50);                          /*Set its size*/
     lv_obj_set_event_cb(btn, btn_event_cb);                 /*Assign a callback to the button*/
 
     lv_obj_t * label = lv_label_create(btn, NULL);          /*Add a label to the button*/
     lv_label_set_text(label, "Button");                     /*Set the labels text*/
+}
+
+lv_chart_series_t * serie_temperature = NULL;
+lv_chart_series_t * serie_humidity = NULL;
+lv_obj_t * chart = NULL;
+void lv_create_chart(){
+	/*Create a chart*/
+    
+    chart = lv_chart_create(lv_scr_act(), NULL);
+    lv_obj_set_size(chart, 200, 150);
+    lv_obj_align(chart, NULL, LV_ALIGN_CENTER, 0, 0);
+    lv_chart_set_type(chart, LV_CHART_TYPE_LINE);   /*Show lines and points too*/
+	lv_chart_set_update_mode(chart, LV_CHART_UPDATE_MODE_SHIFT);
+	lv_chart_set_point_count(chart, 100);
+
+	/*Add a faded are effect*/
+    lv_obj_set_style_local_bg_opa(chart, LV_CHART_PART_SERIES, LV_STATE_DEFAULT, LV_OPA_50); /*Max. opa.*/
+    lv_obj_set_style_local_bg_grad_dir(chart, LV_CHART_PART_SERIES, LV_STATE_DEFAULT, LV_GRAD_DIR_VER);
+    lv_obj_set_style_local_bg_main_stop(chart, LV_CHART_PART_SERIES, LV_STATE_DEFAULT, 255);    /*Max opa on the top*/
+    lv_obj_set_style_local_bg_grad_stop(chart, LV_CHART_PART_SERIES, LV_STATE_DEFAULT, 0);      /*Transparent on the bottom*/
+
+
+	lv_chart_set_y_range(chart, LV_CHART_AXIS_PRIMARY_Y, 20, 35);
+	lv_chart_set_y_range(chart, LV_CHART_AXIS_SECONDARY_Y, 0, 100);
+	//lv_chart_set_x_tick_length(chart, 10, LV_CHART_TICK_LENGTH_AUTO);
+    /*Add two data series*/
+    serie_temperature = lv_chart_add_series(chart, LV_COLOR_GREEN);
+    serie_humidity = lv_chart_add_series(chart, LV_COLOR_BLUE);
+	lv_chart_set_series_axis(chart, serie_humidity, LV_CHART_AXIS_SECONDARY_Y);
 }
 
 void main(void)
@@ -70,48 +100,61 @@ void main(void)
 		LOG_ERR("device not found. Aborting test.");
 		return;
 	}
+	display_set_orientation(display_dev, DISPLAY_ORIENTATION_ROTATED_180);
 
-	hello_world_label = lv_label_create(lv_scr_act(), NULL);
+	/*hello_world_label = lv_label_create(lv_scr_act(), NULL);
 	lv_label_set_text(hello_world_label, "Hello world!");
-	lv_obj_align(hello_world_label, NULL, LV_ALIGN_CENTER, 0, 0);
+	lv_obj_align(hello_world_label, NULL, LV_LABEL_ALIGN_AUTO, 0, 0);*/
 
 	count_label = lv_label_create(lv_scr_act(), NULL);
 	lv_obj_align(count_label, NULL, LV_ALIGN_IN_BOTTOM_MID, 0, 0);
+	lv_label_set_align(count_label, LV_LABEL_ALIGN_CENTER);
 
-	lv_ex_get_started_1();
+	
+
+	/*Image From variable*/
+	lv_obj_t * icon = lv_img_create(lv_scr_act(), NULL);
+	LV_IMG_DECLARE(ESIEALogo);
+	lv_img_set_src(icon, &ESIEALogo);
+	lv_obj_align(icon, NULL, LV_ALIGN_IN_TOP_MID, 0, 0);
+	
+	lv_create_chart();
+	//lv_ex_get_started_1();
 	display_blanking_off(display_dev);
 
-	// BME680
 	const struct device *dev_bme680 = device_get_binding(DT_LABEL(DT_INST(0, bosch_bme680)));
-    // OR const struct device *dev_bme680 = device_get_binding(DT_LABEL(DT_ALIAS(bme680)));
-	struct sensor_value temp, press, humidity, gas_res;
+	struct sensor_value temp, humidity;
+	double temp_value = 0.0;
+	double humidity_value = 0.0;
 
-	printf("Device %p name is %s\n", dev_bme680, dev_bme680->name);
-
-
+	printf("%p\n", dev_bme680);
 
 	while (1) {
-		if ((count % 100) == 0U) {
+		/*if ((count % 100) == 0U) {
 			sprintf(count_str, "%d", count/100U);
 			lv_label_set_text(count_label, count_str);
-			btn_event_cb(btn, LV_EVENT_CLICKED);
-		}
-
-		// BME680
-		sensor_sample_fetch(dev_bme680);
-		sensor_channel_get(dev_bme680, SENSOR_CHAN_AMBIENT_TEMP, &temp);
-		sensor_channel_get(dev_bme680, SENSOR_CHAN_PRESS, &press);
-		sensor_channel_get(dev_bme680, SENSOR_CHAN_HUMIDITY, &humidity);
-		sensor_channel_get(dev_bme680, SENSOR_CHAN_GAS_RES, &gas_res);
-		printf("T: %d.%06d; P: %d.%06d; H: %d.%06d; G: %d.%06d\n",
-				temp.val1, temp.val2, press.val1, press.val2,
-				humidity.val1, humidity.val2, gas_res.val1,
-				gas_res.val2);
-
-		lv_label_set_text_fmt(hello_world_label, "Hello world!: %d.%2d", temp.val1, temp.val2);
-
+			//btn_event_cb(btn, LV_EVENT_CLICKED);
+		}*/
 		lv_task_handler();
 		k_msleep(SLEEP_TIME_MS);
 		++count;
+		if (dev_bme680 != NULL){
+			sensor_sample_fetch(dev_bme680);
+			sensor_channel_get(dev_bme680, SENSOR_CHAN_AMBIENT_TEMP, &temp);
+			sensor_channel_get(dev_bme680, SENSOR_CHAN_HUMIDITY, &humidity);
+			temp_value = sensor_value_to_double(&temp);
+			humidity_value = sensor_value_to_double(&humidity);
+			lv_chart_set_next(chart, serie_temperature, temp_value);
+			lv_chart_set_next(chart, serie_humidity, humidity_value);
+
+			sprintf(count_str, "%d.%02d - %d.%02d", temp.val1, temp.val2/1000, humidity.val1, humidity.val2/1000);
+			lv_label_set_text(count_label, count_str);
+			
+		} else {
+			temp_value = 10+count%25;
+			lv_chart_set_next(chart, serie_temperature, temp_value);
+			sprintf(count_str, "%d", (int)temp_value);
+			lv_label_set_text(count_label, count_str);
+		}
 	}
 }
